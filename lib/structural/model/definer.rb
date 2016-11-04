4a46c76882ca95ed1ab35795c16f7a5c1b90755d
Andrzej Kajetanowicz <Andrzej.Kajetanowicz@onthebeach.co.uk>
2016-11-04 13:50:24 +0000

    Remove redundant #instance_eval call
    Previously methods dynamically defined were using #instance_eval which
    turned out to be redundant. The simplified version of what was happening
    previously could be described as:
    
    ```ruby
    class MyModel
      define_method(:some_name) do
        instance_eval do
          # .. logic goes here
        end
      end
    end
    
    ```
    
    In this case #instance evel is redundant as the block passed to
    `#define_method` is always executed in the context of an instance
    of MyModel class. Additionally, each call to `#instance_eval` allocates
    a new T_NODE object.
    
    In the code used for profiling this change reduces the number of object
    allocations by 8%.

--- lib/structural/model/definer.rb
+++ lib/structural/model/definer.rb
@@ -2,16 +2,16 @@ module Structural
   module Model
     class Definer
       def self.method_memoize(context, name, &value_block)
-        method(context, name) do |object|
-          object.instance_eval do
+        context.class_eval do
+          define_method(name) do
             memoize(name, &value_block)
           end
         end
       end
 
       def self.method(context, name, &value_block)
-        context.instance_eval do
-          define_method name do
+        context.class_eval do
+          define_method(name) do
             value_block.call(self)
           end
         end
